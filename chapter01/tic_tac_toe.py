a6466f0599cfde86460662c18988359f16283552
Yasutaka Odo <YasutakaOdo@gmail.com>
2020-06-22 19:35:56 +0900

    add fibonacchi example for dynamic programming

--- chapter01/tic_tac_toe.py
+++ chapter01/tic_tac_toe.py
@@ -1,3 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.5.0
+#   kernelspec:
+#     display_name: xpython
+#     language: python
+#     name: xpython
+# ---
+
+# %%
 #######################################################################
 # Copyright (C)                                                       #
 # 2016 - 2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)           #
@@ -8,7 +25,7 @@
 # declaration at the top                                              #
 #######################################################################
 
-#%%
+# %%
 import numpy as np
 import pickle 
 
@@ -16,7 +33,7 @@ BOARD_ROWS = 3
 BOARD_COLS = 3
 BOARD_SIZE = BOARD_ROWS * BOARD_COLS
 
-#%%
+# %%
 class State:
     def __init__(self):
         # the board is represented by an n * n array, 
@@ -129,5 +146,276 @@ def get_all_states():
 # all possible board configurations
 all_states = get_all_states()
 
+# %%
+s = State()
+s.print_state()
 
 # %%
+s_new = s.next_state(0, 0, 1)
+s_new.print_state()
+
+# %%
+all_states
+
+# %%
+all_states[16402]
+
+# %%
+all_states[16402][0].print_state()
+
+# %%
+all_states[14764]
+
+# %%
+all_states[14764][0].print_state()
+
+
+# %%
+# AI player
+class Player:
+    # @step_size: the step size to update estimations
+    # @epsilon: the probability to explore
+    def __init__(self, step_size=0.1, epsilon=0.1):
+        self.estimations = dict()
+        self.step_size = step_size
+        self.epsilon = epsilon
+        self.states = []
+        self.greedy = []
+        self.symbol = 0
+
+    def reset(self):
+        self.states = []
+        self.greedy = []
+
+    def set_state(self, state):
+        self.states.append(state)
+        self.greedy.append(True)
+
+    #set up initial values
+    """
+    Assuming we always play Xs, then for all states with three Xs
+    in a row the probability of winning is 1, because we have already 
+    won. Similarly, for all states with three Os in a row, or that are filled 
+    up, the correct probability is 0, as we cannot win from them. 
+    We set the initial values of all the other states to 0.5, representing 
+    a guess that we have a 50% chance of winning.(p9)
+    """
+    def set_symbol(self, symbol):
+        self.symbol = symbol
+        for hash_val in all_states:
+            state, is_end = all_states[hash_val]
+            if is_end:
+                if state.winner == self.symbol:
+                    self.estimations[hash_val] = 1.0
+                elif state.winner == 0:
+                    # we need to distinguish between a tie and a lose
+                    self.estimations[hash_val] = 0.5
+                else:
+                    self.estimations[hash_val] = 0
+            else:
+                self.estimations[hash_val] = 0.5
+        
+    # update value estimation
+    def backup(self):
+        states = [state.hash() for state in self.states]
+
+        for i in reversed(range(len(states) - 1)):
+            state = states[i]
+            td_error = self.greedy[i] * (
+                self.estimations[states[i + 1]] - self.estimations[state]
+            )
+            self.estimations[state] += self.step_size * td_error
+
+    # choose an action based on the state
+    def act(self):
+        state = self.states[-1]
+        next_states = []
+        next_positions = []
+        for i in range(BOARD_ROWS):
+            for j in range(BOARD_COLS):
+                if state.data[i, j] == 0:
+                    next_positions.append([i, j])
+                    next_states.append(state.next_state(
+                        i, j, self.symbol).hash())
+
+        # expolatory move
+        """
+        we select randomly from among the other moves instead. 
+        These are called exploratory moves because they cause 
+        us to experience states that we might otherwise never see.
+        (intro to RL by Sutton p9)
+        """
+        if np.random.rand() < self.epsilon:
+            action = next_positions[np.random.randint(len(next_positions))]
+            action.append(self.symbol)
+            self.greedy[-1] = False
+            return action
+
+        values = []
+        for hash_val, pos in zip(next_states, next_positions):
+            values.append((self.estimations[hash_val], pos))
+        # to select one of the actions of equal value at random due to Python's sort is stable
+        np.random.shuffle(values)
+        values.sort(key=lambda x: x[0], reverse=True)
+        action = values[0][1]
+        action.append(self.symbol)
+        return action
+
+    def save_policy(self):
+        with open('policy_%s.bin' % ('first' if self.symbol == 1 else 'second'), 'wb') as f:
+            pickle.dump(self.estimations, f)
+
+    def load_policy(self):
+        with open('policy_%s.bin' % ('first' if self.symbol == 1 else 'second'), 'rb') as f:
+            self.estimations = pickle.load(f)
+
+# %%
+p1 = Player(step_size=0.1, epsilon=0.1)
+p1.estimations
+
+# %%
+p1.set_symbol(1)
+
+# %%
+p1.estimations
+
+class Judger:
+    # @player1: the player who will move first, its chessman will be 1
+    # @player2: another player with a chessman -1
+    def __init__(self, player1, player2):
+        self.p1 = player1
+        self.p2 = player2
+        self.current_player = None 
+        self.p1_symbol = 1
+        self.p2_symbol = -1
+        self.p1.set_symbol(self.p1_symbol)
+        self.p2.set_symbol(self.p2_symbol)
+        self.current_state = State()
+
+    def reset(self):
+        self.p1.reset()
+        self.p2.reset()
+
+    def alternate(self):
+        while True:
+            yield self.p1
+            yield self.p2
+    
+    # @print_state: if True, print each board during the game
+    def play(self, print_state=False):
+        alternator = self.alternate()
+        self.reset()
+        current_state = State()
+        self.p1.set_state(current_state)
+        self.p2.set_state(current_state)
+        if print_state:
+            current_state.print_state()
+        while True:
+            player = next(alternator)
+            i, j, symbol = player.act()
+            next_state_hash = current_state.next_state(i, j, symbol).hash()
+            current_state, is_end = all_states[next_state_hash]
+            self.p1.set_state(current_state)
+            self.p2.set_state(current_state)
+            if print_state:
+                current_state.print_state()
+            if is_end:
+                return current_state.winner
+
+# %%
+p1 = Player()
+p2 = Player()
+judger = Judger(p1, p2)
+judger.play(print_state=True)
+
+# %% 
+
+
+# %%
+# human interface 
+# input a number to pua chessman
+# | q | w | e |
+# | a | s | d |
+# | z | x | c |
+class HumanPlayer:
+    def __init__(self, **kwargs):
+        self.symbol = None
+        self.keys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']
+        self.state = None
+
+    def reset(self):
+        pass
+
+    def set_state(self, state):
+        self.state = state
+
+    def set_symbol(self, symbol):
+        self.symbol = symbol
+
+    def act(self):
+        self.state.print_state()
+        key = input("Input your position:")
+        data = self.keys.index(key) #keys = ['q', 'w', 'e', ...]
+        i = data // BOARD_COLS 
+        j = data % BOARD_COLS
+        return i, j, self.symbol
+
+def train(epochs, print_every_n=500):
+    player1 = Player(epsilon=0.01)
+    player2 = Player(epsilon=0.01)
+    judger = Judger(player1, player2)
+    player1_win = 0.0
+    player2_win = 0.0
+    for i in range(1, epochs + 1):
+        winner = judger.play(print_state=False)
+        if winner == 1:
+            player1_win += 1
+        if winner == -1:
+            player2_win += 1
+        if i % print_every_n == 0:
+            print('Epoch %d, player 1 winrate: %.02f, player 2 winrate %.02f' % (i, player1_win / i, player2_win / i))
+        player1.backup()
+        player2.backup()
+        judger.reset()
+    player1.save_policy()
+    player2.save_policy()
+
+def compete(turns):
+    player1 = Player(epsilon=0.2)
+    player2 = Player(epsilon=0)
+    judger = Judger(player1, player2)
+    player1.load_policy()
+    player2.load_policy()
+    player1_win = 0.0
+    player2_win = 0.0
+    for _ in range(turns):
+        winner = Judger.play()
+        if winner == 1:
+            player1_win += 1
+        if winner == -1:
+            player2_win += 1
+        judger.reset()
+    print('%d turns, player 1 win %02f, player 2 win %.02f' % (turns, player1_win / turns, player2_win / turns))
+
+# %%
+# The game is a zero sum game. If both players are playing with an optimal strategy, every game will end in a tie. 
+# So we test weather the AI can guarantee at least a tie if it goes second.
+def play():
+    while True:
+        player1 = HumanPlayer()
+        player2 = Player(epsilon=0)
+        judger = Judger(player1, player2)
+        player2.load_policy()
+        winner = judger.play()
+        if winner == player2.symbol:
+            print("You lose!")
+        elif winner == player1.symbol:
+            print("You win!")
+        else:
+            print("It is a tie!")
+
+# %%
+if __name__ == '__main__':
+    train(int(1e5))
+    compete(int(1e3))
+    play()
