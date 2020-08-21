af6815d2aa61a79729242678b1116d0857550aff
Yasutaka Odo <YasutakaOdo@gmail.com>
2020-08-21 22:41:20 +0900

    add codes for chapter 8

---
+++ chapter08/expectation_vs_sample_percent.py
@@ -0,0 +1,56 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)                  #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+%matplotlib inline
+
+# for figure 8.7, run a simulation of 2 * @b steps
+def b_steps(b):
+    # set the value of the next b states
+    # it is not clear how to set this
+    distribution = np.random.randn(b)
+
+    # true value of the current state
+    true_v = np.mean(distribution)
+
+    samples = []
+    errors = []
+
+    # sample 2b steps
+    for t in range(2 * b):
+        v = np.random.choice(distribution)
+        samples.append(v)
+        errors.append(np.abs(np.mean(samples) - true_v))
+
+    return errors
+
+# %%
+def figure_8_7():
+    runs = 100
+    branch = [2, 10, 100, 1000]
+    for b in branch:
+        errors = np.zeros((runs, 2 * b))
+        for r in tqdm(np.arange(runs)):
+            errors[r] = b_steps(b)
+        errors = errors.mean(axis=0)
+        x_axis = (np.arange(len(errors)) + 1) / float(b)
+        plt.plot(x_axis, errors, label='b = %d' % (b))
+
+    plt.xlabel('number of computations')
+    plt.xticks([0, 1.0, 2.0], ['0', 'b', '2b'])
+    plt.ylabel('RMS error')
+    plt.legend()
+
+    plt.show()
+    plt.close()
+
+# %%
+figure_8_7()
